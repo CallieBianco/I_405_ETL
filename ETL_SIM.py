778d077b2537f4f1ad0ab37fdf4d58424bb7e762
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-05-30 12:32:36 -0700

    Update ETL_SIM.py

--- ETL_SIM.py
+++ ETL_SIM.py
@@ -10,6 +10,7 @@ from highway import Highway
 from car import Car
 from bus import Bus
 import random
+import file_saver
 
 min_price = 1000
 max_price = 1000
@@ -37,23 +38,25 @@ for m in range(min_price):
         for c in range(sim_number):
             north_highway = Highway(length_highway)
             south_highway = Highway(length_highway, min_toll=m, max_toll=n)
-            vehicle_list = []
+            n_vehicle_list = []
+            s_vehcile_list
             #North Highway
             for t in range(time_range):
                 total_moved_per_step = numpy.zeros((north_highway.width))
                 while bool(random.getrandbits(1)):
                     if random.randint(0, 1000000) / 1000000.0 <= percent_bus:
                         enter_number = random.randint(0, len(north_highway.entrance_arr))
-                        vehicle_list.append(Bus(3, north_highway.entrance_arr[enter_number], north_highway.grids_per_mile))
+                        n_vehicle_list.append(Bus(3, north_highway.entrance_arr[enter_number], north_highway.grids_per_mile))
                     else:
-                        vehicle_list.append(Car())
+                        n_vehicle_list.append(Car())
                 shift = 0
-                for i in range(len(vehicle_list)):
+                for i in range(len(n_vehicle_list)):
                     i -= shift
-                    grids_squares_moved, north_highway, exited = vehicle_list[i].move(north_highway)
+                    grids_squares_moved, north_highway, exited = n_vehicle_list[i].move(north_highway)
                     total_moved_per_step[vehicle_list[i].x-1] += grids_squares_moved
                     if exited == True:
-                        vehicle_list.remove(i)
+                        north_highway.exits[(n_vehicle_list[i].exit)].intake(n_vehicle_list[i])                       
+                        n_vehicle_list.remove(i)
                         shift += 1
                 for i in range(len(north_highway.exits_arr)):
                     north_highway.exits_arr.deplete()
@@ -64,15 +67,16 @@ for m in range(min_price):
                 while bool(random.getrandbits(1)):
                     if random.randint(0, 1000000) / 1000000.0 <= percent_bus:
                         enter_number = random.randint(0, len(south_highway.entrance_arr))
-                        vehicle_list.append(Bus(3, north_highway.entrance_arr[enter_number], south_highway.grids_per_mile))
+                        s_vehicle_list.append(Bus(3, north_highway.entrance_arr[enter_number], south_highway.grids_per_mile))
                     else:
-                        vehicle_list.append(Car())
+                        s_vehicle_list.append(Car())
                 shift = 0
                 for i in range(len(vehicle_list)):
                     i -= shift
-                    grids_squares_moved, south_highway, exited = vehicle_list[i].move(south_highway)
-                    if exited == True:
-                        vehicle_list.remove(i)
+                    grids_squares_moved, south_highway, exited = s_vehicle_list[i].move(south_highway)
+                    if exited == True:                        
+                        south_highway.exits[(s_vehicle_list[i].exit)].intake(s_vehicle_list[i]) 
+                        s_vehicle_list.remove(i)
                         shift += 1
                 for i in range(len(north_highway.exits_arr)):
                     north_highway.exits_arr.deplete()        
