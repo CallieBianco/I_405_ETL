ca4b22e9df1f6966b01abd4fb3736f23e016fe7d
CallieBianco <36008290+CallieBianco@users.noreply.github.com>
2019-05-30 22:35:31 -0700

    Finalized Sensitivity Analysis
    Will add more one thing to analyze the model tomorrow and then this portion will be finished.

--- Price_Elasticity_Model.py
+++ Price_Elasticity_Model.py
@@ -1,7 +1,8 @@
 from car import Car
 import numpy as np
+import matplotlib.pyplot as plt
 
-def price_elasticity_weights(props, tol):
+def price_elasticity_weights(props):
     """ Determines if the set of weights is valid
     """
     # determine most appropriate weights
@@ -30,7 +31,7 @@ def price_elasticity_weights(props, tol):
     N_NON = np.arange(START, 16)
     TOTAL_CARS = (NUM_CARS_PEAK*(len(S_PEAK))) + (NUM_CARS_NON_PEAK* \
                   (len(S_NON)))
-    good_count = 0
+    diff = 0
     for n in range(10):
         count = 0
         for i in range(len(S_PEAK)):
@@ -45,15 +46,22 @@ def price_elasticity_weights(props, tol):
                 if sc.want_to_move_to_ETL(NON, S_NON[i], NON_ETL_SPEED, \
                                         NON_GPL_SPEED, props) == True:
                     count += 1
-        if expected - tol < (count / TOTAL_CARS) < expected + tol:
-            good_count += 1
-    print(str(props) + ": " + str(good_count))
-    if good_count >= 7:
-        print("Good weights")
-        print(props)
-        return -1
-    else:
-        return good_count
+        for i in range(len(N_PEAK)):
+            for j in range(NUM_CARS_PEAK):
+                sc = Car(direction='North')
+                if sc.want_to_move_to_ETL(PEAK, N_PEAK[i], PEAK_ETL_SPEED, \
+                                        PEAK_GPL_SPEED, props) == True:
+                    count += 1
+        for i in range(len(N_NON)):
+            for j in range(NUM_CARS_NON_PEAK):
+                sc = Car(direction='North')
+                if sc.want_to_move_to_ETL(NON, N_NON[i], NON_ETL_SPEED, \
+                                        NON_GPL_SPEED, props) == True:
+                    count += 1
+        total = count / (TOTAL_CARS*2)
+        diff += abs(expected - total)
+    return diff / 10
+    
         
 def expected_proportion_use():
     """ Returns the expected proportion of cars that use the ETLs
@@ -76,11 +84,17 @@ def expected_proportion_use():
     return (south_proportion + north_proportion) / 2   
 
 def find_best_weight():
-    """ Work in progress.
+    """ Determines the best score weights by finding the set of weights
+        that make the proportion of ETL cars to GPL cars as close to 
+        expected as possible.
+        
+        Note: Once I run it and find the best set of weights, I will take note 
+              of it as the set to use; I don't want to run this function every
+              time I run the want_to_move_to_ETL() function for time sake.
     """
     # order:
     # inc_score, time_score, commuter_score, gtg_score, hurry_score, speed_score
-    # parameters:
+    # Weight distribution rules:
     # all values 1-10
     # inc_score is always at least the highest (can be tied)
     # hurry_score and speed_score are the next highest (can be flipped or tied)
@@ -93,20 +107,172 @@ def find_best_weight():
     hs = np.random.randint(5, 10, 100)
     ss = np.random.randint(5, 10, 100)
     weights = np.zeros(len(ins))
+    # test different weighted combinations adhering to weight distribution rules
     for i in range(len(ins)):
         weight = np.array([ins[i], ts[i], cs[i], gs[i], hs[i], ss[i]])
         diff = price_elasticity_weights(weight)
+        # find difference in actual value vs. expected
         weights[i] = diff
+    # return index where the difference is the smallest
     min_i = np.argmin(weights)
-    print(price_elasticity_weights([ins[min_i], ts[min_i], cs[min_i], \
-           gs[min_i], hs[min_i], ss[min_i]]))
+    # make sure income score is the highest (most important) weight
+    if ins[min_i] < ss[min_i]:
+        temp = ss[min_i]
+        ss[min_i] = ins[min_i]
+        ins[min_i] = temp
     return [ins[min_i], ts[min_i], cs[min_i], gs[min_i], hs[min_i], ss[min_i]]
 
-best_weight = find_best_prop()
-if best_weight[-1] > best_weight[0]:
-    temp = best_weight[-1]
-    best_weight[-1] = best_weight[0]
-    best_weight[0] = temp
-print(best_weight)
+#best_weight = find_best_weight()
+#best_weight = [8, 2, 3, 3, 6, 8]
+#expected - actual = .0055789
+
+def speed_sensitivity():
+    """ Analyzes how proportion of cars in ETLs vs GPL changes as speed 
+        differences in the lanes change.
+
+    """ 
+    # use average tolls to test: $5 during peak and $1.25 in non-peak
+    PEAK = 5
+    NON = 1.25
+    NUM_CARS_NON_PEAK = 100
+    NUM_CARS_PEAK = 200
+    # simulate tolling hours (non-tolling hours, ETLs act as regular lanes)
+    START = 5
+    END = 19
+    # change speeds (as speeds in ETL get faster, number of cars wanting to move
+    #                should increase)
+    PEAK_ETL_SPEED = np.arange(30, 61)
+    # will stay constant
+    PEAK_GPL_SPEED = 30
+    # keep at average
+    NON_ETL_SPEED = 60
+    NON_GPL_SPEED = 55
+    S_PEAK = np.arange(START, 10)
+    N_PEAK = np.arange(15, END+1)
+    S_NON = np.arange(10, END)
+    N_NON = np.arange(START, 15)
+    TOTAL_CARS = (NUM_CARS_PEAK*(len(S_PEAK))) + (NUM_CARS_NON_PEAK* \
+                  (len(S_NON)))
+    changes_s = np.zeros(len(PEAK_ETL_SPEED))
+    changes_n = np.zeros(len(PEAK_ETL_SPEED))
+    SIM = 100
+    """
+    for k in range(SIM):
+        for n in range(len(PEAK_ETL_SPEED)):
+            count = 0
+            for i in range(len(S_PEAK)):
+                for j in range(NUM_CARS_PEAK):
+                    sc = Car(direction='South')
+                    if sc.want_to_move_to_ETL(PEAK, S_PEAK[i], PEAK_ETL_SPEED[n], \
+                                            PEAK_GPL_SPEED) == True:
+                        count += 1
+            for i in range(len(S_NON)):
+                for j in range(NUM_CARS_NON_PEAK):
+                    sc = Car(direction='South')
+                    if sc.want_to_move_to_ETL(NON, S_NON[i], NON_ETL_SPEED, \
+                                              NON_GPL_SPEED) == True:
+                        count += 1
+            changes_s[n] += count / TOTAL_CARS
+    changes_s /= SIM
+    """
+    for k in range(SIM):
+        for n in range(len(PEAK_ETL_SPEED)):
+            count = 0
+            for i in range(len(N_PEAK)):
+                for j in range(NUM_CARS_PEAK):
+                    nc = Car(direction='North')
+                    if nc.want_to_move_to_ETL(PEAK, N_PEAK[i], PEAK_ETL_SPEED[n], \
+                                              PEAK_GPL_SPEED) == True:
+                        count += 1
+            for i in range(len(N_NON)):
+                for j in range(NUM_CARS_NON_PEAK):
+                    nc = Car(direction='North')
+                    if nc.want_to_move_to_ETL(NON, N_NON[i], NON_ETL_SPEED, \
+                                              NON_GPL_SPEED) == True:
+                        count += 1
+            changes_n[n] += count / TOTAL_CARS
+    changes_n /= SIM
+    
+    plt.plot(PEAK_ETL_SPEED - 30, changes_n)
+    plt.title("Northbound: Proportion of Total Cars in ETLs as \n \
+              Speed Difference Increases")
+    plt.xlabel("MPH Faster than GPL")
+    plt.ylabel("Proportion of Cars in ETL")
+    plt.show()   
+    
+def price_sensitivity():
+    """ Analyzes how proportion of cars in ETLs vs GPL changes as toll price
+        changes.
+        
+    """ 
+    # Change peak prices
+    PEAK = np.arange(.75, 10, .25)
+    # keep at average
+    NON = 1.25
+    NUM_CARS_NON_PEAK = 100
+    NUM_CARS_PEAK = 200
+    # simulate tolling hours (non-tolling hours, ETLs act as regular lanes)
+    START = 5
+    END = 19
+    # Use average speeds (northbound is same proportion as southbound)
+    PEAK_ETL_SPEED = 39
+    PEAK_GPL_SPEED = 25
+    NON_ETL_SPEED = 60
+    NON_GPL_SPEED = 55
+    S_PEAK = np.arange(START, 10)
+    N_PEAK = np.arange(15, END+1)
+    S_NON = np.arange(10, END)
+    N_NON = np.arange(START, 15)
+    TOTAL_CARS = (NUM_CARS_PEAK*(len(S_PEAK))) + (NUM_CARS_NON_PEAK* \
+                  (len(S_NON)))
+    #for n in range(10):
+    changes_s = np.zeros(len(PEAK))
+    changes_n = np.zeros(len(PEAK))
+    SIM = 100
+    """
+    for k in range(SIM):
+        for n in range(len(PEAK)):
+            count = 0
+            for i in range(len(S_PEAK)):
+                for j in range(NUM_CARS_PEAK):
+                    sc = Car(direction='South')
+                    if sc.want_to_move_to_ETL(PEAK[n], S_PEAK[i], PEAK_ETL_SPEED, \
+                                            PEAK_GPL_SPEED) == True:
+                        count += 1
+            for i in range(len(S_NON)):
+                for j in range(NUM_CARS_NON_PEAK):
+                    sc = Car(direction='South')
+                    if sc.want_to_move_to_ETL(NON, S_NON[i], NON_ETL_SPEED, \
+                                              NON_GPL_SPEED) == True:
+                        count += 1
+            changes_s[n] += count / TOTAL_CARS
+    changes_s /= SIM
+    """
+    for k in range(SIM):
+        for n in range(len(PEAK)):
+            count = 0
+            for i in range(len(N_PEAK)):
+                for j in range(NUM_CARS_PEAK):
+                    nc = Car(direction='North')
+                    if nc.want_to_move_to_ETL(PEAK[n], N_PEAK[i], PEAK_ETL_SPEED, \
+                                              PEAK_GPL_SPEED) == True:
+                        count += 1
+            for i in range(len(N_NON)):
+                for j in range(NUM_CARS_NON_PEAK):
+                    nc = Car(direction='North')
+                    if nc.want_to_move_to_ETL(NON, N_NON[i], NON_ETL_SPEED, \
+                                              NON_GPL_SPEED) == True:
+                        count += 1
+            changes_n[n] += count / TOTAL_CARS
+    changes_n /= SIM
+    
+    plt.plot(PEAK, changes_n, 'r')
+    plt.title("Northbound: Proportion of Total Cars in ETLs as \n \
+              ETL Toll Price Increases")
+    plt.xlabel("Toll Price ($)")
+    plt.ylabel("Proportion of Cars in ETL")
+    plt.show() 
+#speed_sensitivity()
+#price_sensitivity()
     
     
