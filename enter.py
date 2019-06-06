2149f75d40508fda94d9e1040793fed14e12cb2b
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-06-05 20:40:55 -0700

    Update enter.py

--- enter.py
+++ enter.py
@@ -1,12 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 22 10:09:02 2019
+#=======================================================================
+#                        General Documentation
+#
+    # Entrance Ramp for I-405 Simulation
+#
+#-----------------------Additional Documentation------------------------
 
-@author: awsir
-"""
+# Modification History:
+# - 19 May 2019: enter.py created by Adam Sirkis
+# - 04 June 2019: enter.py finalized by Adam Sirkis
+
+# Notes:
+# - Developed for Python 3.x
+
+#=======================================================================
 
 class Enter:
-    def __init__(self, number, grids_per_mile, y, max_capacity=25,):   
+    def __init__(self, number, grids_per_mile, y, max_capacity=25,): 
+        
+        """ COnstructor for Entrance
+
+        
+
+        Method Arguments:
+
+            - Number : a number representing the exit number relative to the entrance
+
+            - grids_per_mile : the number fo grid squares per distance of the highway
+
+            - y : number representing the coordinate lengthwise fo the exit
+
+            - max_capacity  numbebr of vehicles it can hold
+
+            
+
+           """
         self.max = max_capacity
         self.count = 0
         self.number_dispensed = 0
@@ -16,9 +43,45 @@ class Enter:
         self.y = y
         
     def intake(self, veh):
+    
+        """
+
+        Take in vehicles
+
+        
+
+        Method arguments: 
+
+            - veh : the vehicle to take in
+
+        
+
+        Return Values:
+
+            - Bool representing if the inatek was successful
+
+            """
         self.count += 1
     
     def deplete(self):
+    
+        """
+
+        Push out vehicles
+
+        
+
+        Method arguments: 
+
+            - none
+
+        
+
+        Return Values:
+
+            - none
+
+            """
         if self.count >= self.dispense_num:
             self.count -= self.dispense_num
             self.number_dispensed += self.dispense_num
