53e5418ffeecda96f166140b699126b84bcc4e52
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-06-05 20:39:08 -0700

    Update exit.py

--- exit.py
+++ exit.py
@@ -1,12 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 22 10:08:55 2019
+#=======================================================================
+#                        General Documentation
+#
+    # Exit Object for I-405 Simulation
+#
+#-----------------------Additional Documentation------------------------
 
-@author: awsir
-"""
+# Modification History:
+# - 19 May 2019: exit.py created by Adam Sirkis
+# - 04 June 2019: exit.py finalized by Adam Sirkis
 
+# Notes:
+# - Developed for Python 3.x
+
+#=======================================================================
 class Exit:
     def __init__(self, number, grids_per_mile, y, max_capacity=10):    
+        """ COnstructor for Exit
+        
+        Method Arguments:
+            - Number : a number representing the exit number relative to the entrance
+            - grids_per_mile : the number fo grid squares per distance of the highway
+            - y : number representing the coordinate lengthwise fo the exit
+            - max_capacity  numbebr of vehicles it can hold
+            
+           """
         self.max = max_capacity
         self.count = 0
         self.number_dispensed = 0
@@ -15,6 +32,15 @@ class Exit:
         self.y = y
         
     def intake(self, veh):
+        """
+        Take in vehicles
+        
+        Method arguments: 
+            - veh : the vehicle to take in
+        
+        Return Values:
+            - Bool representing if the inatek was successful
+            """
         if self.count < self.max:
             self.count += 1
             return True
@@ -22,6 +48,15 @@ class Exit:
             return False
     
     def deplete(self):
+        """
+        Push out vehicles
+        
+        Method arguments: 
+            - none
+        
+        Return Values:
+            - none
+            """
         if self.count >= self.dispense_num:
             self.count -= self.dispense_num
             self.number_dispensed += self.dispense_num
